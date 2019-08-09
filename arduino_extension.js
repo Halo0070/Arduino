3240068f8dda74ed06883b7330c91979d574e33b
Halo0070 <lee998598@gmail.com>
2019-08-09 13:43:43 +0900

    Update arduino_extension.js

--- arduino_extension.js
+++ arduino_extension.js
@@ -296,6 +296,7 @@
     return (digitalInputData[pin >> 3] >> (pin & 0x07)) & 0x01;
   }
 
+  /*
   function analogWrite(pin, val) {
     if (!hasCapability(pin, PWM)) {
       console.log('ERROR: valid PWM pins are ' + pinModes[PWM].join(', '));
@@ -311,6 +312,23 @@
         val >> 7]);
     device.send(msg.buffer);
   }
+  */
+
+ function analogWrite(pin, val) {
+  if (!hasCapability(pin, PWM)) {
+    console.log('ERROR: valid PWM pins are ' + pinModes[PWM].join(', '));
+    return;
+  }
+  if (val < 0) val = 0;
+  else if (val > 100) val = 1000;
+  val = Math.round((val / 1000) * 255);
+  pinMode(pin, PWM);
+  var msg = new Uint8Array([
+      ANALOG_MESSAGE | (pin & 0x0F),
+      val & 0x7F,
+      val >> 7]);
+  device.send(msg.buffer);
+}
 
   function digitalWrite(pin, val) {
     if (!hasCapability(pin, OUTPUT)) {
@@ -539,6 +557,10 @@
     analogWrite(3, 0);
     analogWrite(11, 0);
   };
+
+  tonemap: {
+
+  }
 
   ext.buzzer = function(pin, val) {
     /*
